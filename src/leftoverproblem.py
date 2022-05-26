3a2d1696bfaa1db29ad971e681ce5f93a4389572
Esdras Battosti <esdrasbattostisilva@gmail.com>
2022-05-26 20:50:08 -0300

    switching comma to "and"

--- src/leftoverproblem.py
+++ src/leftoverproblem.py
@@ -1,6 +1,6 @@
 """
 Copyright 2022 Glaucia Maria Bressan, Esdras Battosti da Silva,
-Matheus Henrique Pimenta-Zanon, Elisângela Aparecida da Silva Lizzi
+Matheus Henrique Pimenta-Zanon and Elisângela Aparecida da Silva Lizzi
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
